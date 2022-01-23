b050251f91026ee09a948296b328551a78c5f8a6
miss-gif <ddc.akari@gmail.com>
2022-01-23 23:22:23 +0900

    badges

--- js/main.js
+++ js/main.js
@@ -14,3 +14,26 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // badgeEl.style.display = "none";
+      // gsap.to(요소, 지속시간, 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // badgeEl.style.display = "block";
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+); // _.throttle(함수, 시간) 300 = 0.3초, 해당 단위로 함수가 실행됨.
