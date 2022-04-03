a9507a6e5599780a41c4f2b6bf6380f2ea79cd53
miss-gif <ddc.akari@gmail.com>
2022-04-03 23:22:48 +0900

    weather.js add

--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,25 @@
-
-
-getCurrentPosition(성공, 실패)의 역활
-
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
 
+const API_KEY = "705cc22546b2ab62a3f9dce86744cd06";
+
 function onGeoOk(position) {
-  const lat = ;
-  const lng = ;
-  console.log(position)
+  const lat = position.coords.latitude; // latitude 위도
+  const lng = position.coords.longitude; // longitude 경도
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  console.log(url);
+
+  fetch(url);
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = date.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
-alert("위치 추적 실패")
+  alert("위치 추적 실패");
 }
