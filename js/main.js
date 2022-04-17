6f16b16ce086b5e150bb4d516491de2c23e7cda5
miss-gif <ddc.akari@gmail.com>
2022-04-18 04:14:00 +0900

    JS FIX

--- js/main.js
+++ js/main.js
@@ -1,39 +1,40 @@
+// 배열 데이터
 const pokemons = [
   {
     id: 1,
-    name: 1,
-    type: 1,
-    url: 1,
+    name: "이상해씨",
+    type: "풀, 독",
+    url: "./img/1.png",
   },
   {
     id: 2,
-    name: 2,
-    type: 1,
-    url: 1,
+    name: "이상해풀",
+    type: "풀, 독",
+    url: "./img/2.png",
   },
   {
     id: 3,
-    name: 3,
-    type: 1,
-    url: 1,
+    name: "이상해꽃",
+    type: "풀, 독",
+    url: "./img/3.png",
   },
   {
     id: 4,
-    name: 4,
-    type: 1,
-    url: 1,
+    name: "메가 이상해꽃",
+    type: "풀, 독",
+    url: "./img/4.png",
   },
 ];
 
-const list = document.querySelector("#list");
-
+// 배열 데이터로 list 생성
+const list = document.getElementById("list");
 function showList(val = "") {
   list.innerHTML = "";
   const res = pokemons.forEach((pokemon) => {
-    if (pokemon.name) {
-      // <- 이부분 에러
+    if (pokemon.name.includes(val)) {
       const li = document.createElement("li");
       li.innerHTML = `
+      <img src='${pokemon.url}' alt='${pokemon.name}'>
       <p>이름: ${pokemon.name}</p>
       <p>속성: ${pokemon.type}</p>
       `;
@@ -43,9 +44,9 @@ function showList(val = "") {
 }
 showList();
 
-// 검색기능
-const searchInput = document.querySelector("#search");
-const searchBtn = document.querySelector("#searchBtn");
+// 검색 기능
+const searchInput = document.getElementById("search");
+const searchBtn = document.getElementById("searchBtn");
 
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
